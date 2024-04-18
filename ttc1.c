1e370313783cebafea7592c8da9e5ffcee512c71
SAMRIDHEE GHIMIRE <164874102+samridheeghimire@users.noreply.github.com>
2024-04-18 16:18:14 +0545

    Update ttc1.c

--- ttc1.c
+++ ttc1.c
@@ -7,7 +7,7 @@ void clearScreen(); // Declare clearScreen function
 
 char board[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-void main()
+int main()
 {
     char p1[30], p2[30];
     printf("Enter first player name\n");
